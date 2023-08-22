2feecde025751a40bce315ae67df7dc630edc9a8
Kuanysh23524 <kukaomar011@mail.ru>
2023-08-22 23:50:01 +0600

    spring-sprint-task-2

--- src/main/java/com/example/springsprinttask2/controllers/HomeController.java
+++ src/main/java/com/example/springsprinttask2/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.springsprinttask2.controllers;
 
 import com.example.springsprinttask2.models.ApplicationRequest;
+import com.example.springsprinttask2.repositories.ApplicationReqRep;
 import com.example.springsprinttask2.services.ApplicationReqService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.function.LongToIntFunction;
 
@@ -15,6 +17,8 @@ import java.util.function.LongToIntFunction;
 public class HomeController {
     @Autowired
     ApplicationReqService applicationReqService;
+    @Autowired
+    ApplicationReqRep applicationReqRep;
 
 
     @GetMapping("/")
@@ -22,6 +26,14 @@ public class HomeController {
         model.addAttribute("info", applicationReqService.getAppReq());
         return "home";
     }
+
+
+//@GetMapping("/search")
+//public String searchPage(@RequestParam(name = "search") String search ,Model model) {
+//    System.out.println(search);
+//    model.addAttribute("items",applicationReqRep.findAllByUserName(search));
+//    return "home";
+//}
 
     @GetMapping("/Add-Page")
     public String addPage() {
--- src/main/java/com/example/springsprinttask2/repositories/ApplicationReqRep.java
+++ src/main/java/com/example/springsprinttask2/repositories/ApplicationReqRep.java
@@ -2,8 +2,18 @@ package com.example.springsprinttask2.repositories;
 
 import com.example.springsprinttask2.models.ApplicationRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ApplicationReqRep extends JpaRepository<ApplicationRequest, Long> {
+    //    @Query("select  i from ApplicationRequest i "
+//            + "where i.userName ilike concat('%', :search, '%') "
+//            + "or i.courseName ilike concat('%', :search, '%')"
+//    )
+//
+//    List<ApplicationRequest> search(String search);
+//    List<ApplicationRequest> findAllByUserName(String userName);
 }
